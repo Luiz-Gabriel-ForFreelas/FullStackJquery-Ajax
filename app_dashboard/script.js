ba9663f7b252b6aa1ad86fd08f876635e71cb7dc
unknown <luiz.g.silva4@gmail.com>
2025-03-06 14:23:33 -0300

    added the get method to superimpose the html from div (id=page) with ajax

--- app_dashboard/script.js
+++ app_dashboard/script.js
@@ -1,3 +1,22 @@
 $(document).ready(() => {
-	
+    $('#documentation').on('click', () => {
+        // Utilizando o load() e o get()
+
+        // $('#page').load('documentacao.html')
+        
+        $.get('documentacao.html', data => {
+            $('#page').html(data)
+        })
+    })	
+
+    $('#suport').on('click', () => {
+        // Utilizando o load() e o get()
+
+        // $.get('suporte.html', data => {
+        //     $('#page').html(data)
+        // })
+
+        $('#page').load('suporte.html')
+
+    })
 })
